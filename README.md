f91e3c61bb8e194364ed78fea7435c79cb89514c
Amelie Biwole <amelie.biwole@yahoo.com>
2024-10-16 15:22:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a basic Website made with HTML and CSS as well as JavaScript and Bootstrap components for interactivity.
+
+JavaScript usage:
+- i used vanilla js to make a little quiz about me and to keep track of the total score
+- if the user gets everything right I used css-animation to make  blinking text
+
+Bootstrap usage:
+- all pasges: sticky navbar element for navigating the website, with a little website icon included
+- projects page: cards with image, little description and link to GitHub
+- cv page: collapsable accordion to focus on a specific section
+- all bootstrap components have been modified with css containers
---
+++ js/scriptQuiz.js
@@ -0,0 +1,46 @@
+let counter = 0;
+
+function findParentByClass(element, className) {
+    if (!element || !element.parentElement) {
+        return null;
+    }
+    if (element.parentElement.classList.contains(className)) {
+        return element.parentElement;
+    }
+    return findParentByClass(element.parentElement, className);
+}
+
+function checkMultiChoice(event) {
+    let button = event.target;
+    if (button.classList.contains('correct')) {
+        let otherButtons = button.parentElement.querySelectorAll('.incorrect');
+        otherButtons.forEach(function(b) {
+            b.classList.add('not-clickable');
+        });
+        button.parentElement.querySelector('.feedback').innerHTML = 'Correct!';
+        counter++;
+    }
+    else {
+        let buttons = button.parentElement.querySelectorAll('button');
+        buttons.forEach(function(b) {
+            if (b != button) {
+                b.classList.add('not-clickable');
+            }
+        });
+        let correctButton = button.parentElement.querySelector('.correct');
+        button.parentElement.querySelector('.feedback').innerHTML = 'Incorrect: Correct answer is '+ correctButton.innerHTML +'!';
+    }
+
+    divElement = findParentByClass(button, 'custom-text-block-Quiz');
+    if (divElement) {
+        divElement.querySelector('#result').innerHTML = counter + ' out of ' + divElement.querySelectorAll('.section').length + ' right';
+        
+        // check if all questions are answered right, if yes blink the result
+        if (counter == divElement.querySelectorAll('.section').length) {
+        res = document.getElementById('result');
+        res.innerHTML = 'Wow, all correct!';
+        res.style.fontSize = '1.5em';
+        res.classList.add('blinking-text');
+        }
+    }
+}
